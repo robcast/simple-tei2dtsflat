7013fc92c804e0a98abc6ae690516915b992e2d1
Robert Casties <casties@mpiwg-berlin.mpg.de>
2022-02-08 12:24:34 +0100

    writes document structure and half of navigation structure now.

--- tei2dtsflat.py
+++ tei2dtsflat.py
@@ -6,6 +6,7 @@ import time
 import xml.etree.ElementTree as ET
 from pathlib import Path
 import xml
+import json
 
 XMLNS = {'': 'http://www.tei-c.org/ns/1.0',
          'xml': 'http://www.w3.org/XML/1998/namespace',
@@ -18,28 +19,30 @@ def ns_name(ns, name):
     return f"{{{XMLNS[ns]}}}{name}"
 
 
-def write_document(doc, basedir):
+def write_xml_document(doc, args):
     """
-    Write document doc as XML file in basedir.
+    Write document doc as XML file in basedir/docid.
     """
-    dir = Path(basedir)
+    dir = Path(args.basedir, args.docid)
     dir.mkdir(parents=True, exist_ok=True)
     outfile = Path(dir, 'tei-doc.xml')
+    logging.debug(f"writing XML document {outfile}")
     with outfile.open(mode='wb') as f:
         # write as new ElementTree
         tree = ET.ElementTree(doc)
         tree.write(f, encoding='utf-8', xml_declaration=True)        
 
 
-def write_fragment(doc, basedir, level, frag_id, args):
+def write_xml_fragment(doc, frag_id, args):
     """
     Write fragment doc as XML file in structured directories starting at basedir.
     
-    Directory structure: basedir/level/frag_id/tei-frag.xml
+    Directory structure: basedir/docid/frag_id/tei-frag.xml
     """
-    dir = Path(basedir, str(level), frag_id)
+    dir = Path(args.basedir, args.docid, frag_id)
     dir.mkdir(parents=True, exist_ok=True)
     outfile = Path(dir, 'tei-frag.xml')
+    logging.debug(f"writing XML fragment {outfile}")
     with outfile.open(mode='wb') as f:
         # root element TEI
         tei = ET.Element('TEI')
@@ -50,6 +53,22 @@ def write_fragment(doc, basedir, level, frag_id, args):
         tree = ET.ElementTree(tei)
         tree.write(f, encoding='utf-8', xml_declaration=True)
         
+
+def write_json_document(doc, level, args):
+    """
+    Write document doc as JSON file in basedir/docid.
+    """
+    if level == 0:
+        dir = Path(args.basedir, args.docid)
+    else:
+        dir = Path(args.basedir, args.docid, str(level))
+        
+    dir.mkdir(parents=True, exist_ok=True)
+    outfile = Path(dir, 'dts-nav.json')
+    logging.debug(f"writing JSON document {outfile}")
+    with outfile.open(mode='w', encoding='utf-8') as f:
+        json.dump(doc, f)       
+
 
 def load_xml_file(args):
     """
@@ -64,7 +83,7 @@ def load_xml_file(args):
     return tree.getroot()
 
 
-def parse_div(doc, level, args):
+def parse_tei_div(doc, level, args):
     """
     Parse TEI div structure in doc Element (recursively). 
     
@@ -73,11 +92,14 @@ def parse_div(doc, level, args):
     """
     div_id = doc.get(ns_name('xml', 'id'))
     if div_id is None:
-        div_id = f"id-div{args.id_cnt}"
-        args.id_cnt += 1
+        # create and set new id
+        div_id = f"{args.genid_prefix}-div{args.genid_cnt}"
+        args.genid_cnt += 1
         doc.set(ns_name('xml', 'id'), div_id)
         
     div_type = doc.get('type')
+    
+    # collect the text from all head tags
     headtexts = []
     heads = doc.findall('head', XMLNS)
     if heads:
@@ -87,17 +109,16 @@ def parse_div(doc, level, args):
     headtext = ' '.join(headtexts)
     
     # write div fragment to file
-    write_fragment(doc, args.basedir, level, div_id, args)
+    write_xml_fragment(doc, div_id, args)
     
-    # process sub-divs
+    # collect all sub-divs
     subdivs = doc.findall('div', XMLNS)
     subdiv_infos = []
     if subdivs:
         for subdiv in subdivs:
-            subdiv_infos.append(parse_div(subdiv, level+1, args))
+            subdiv_infos.append(parse_tei_div(subdiv, level+1, args))
         
-    logging.debug(f"div level={level} id={div_id} type={div_type}")
-    logging.debug(f"  head: {headtext}")
+    logging.debug(f"div level={level} id={div_id} type={div_type} heads: {headtext}")
     
     info = {
         'level': level,
@@ -109,7 +130,7 @@ def parse_div(doc, level, args):
     return info
 
 
-def parse_tei(doc, args):
+def parse_tei_doc(doc, args):
     """
     Parse TEI document in doc Element.
     
@@ -123,26 +144,107 @@ def parse_tei(doc, args):
         raise RuntimeError("Not a valid TEI document: missing 'text' element")
     
     # write full document to basedir
-    write_document(doc, args.basedir)
+    write_xml_document(doc, args)
     
     infos = []
     front = text.find('front', XMLNS)
     if front:
         for div in front.findall('div', XMLNS):
-            infos.append(parse_div(div, 1, args))
+            infos.append(parse_tei_div(div, 1, args))
 
     body = text.find('body', XMLNS)
     if body:
         for div in body.findall('div', XMLNS):
-            infos.append(parse_div(div, 1, args))
+            infos.append(parse_tei_div(div, 1, args))
 
     back = text.find('back', XMLNS)
     if back:
         for div in back.findall('div', XMLNS):
-            infos.append(parse_div(div, 1, args))
+            infos.append(parse_tei_div(div, 1, args))
 
     return infos
 
+
+def get_div_maxlevel(infos, maxlevel):
+    """
+    Returns the maximum div level.
+    """
+    for info in infos:
+        if info['level'] > maxlevel:
+            maxlevel = info['level']
+            
+        if info['subdivs']:
+            maxlevel = get_div_maxlevel(info['subdivs'], maxlevel)
+            
+    return maxlevel
+
+
+def get_div_ids(infos, level):
+    """
+    Returns a list of all div ids at level.
+    """
+    ids = []
+    for info in infos:
+        if info['level'] == level:
+            ids.append(info['id'])
+            
+        if info['subdivs']:
+            subids = get_div_ids(info['subdivs'], level)
+            ids.extend(subids)
+            
+    return ids
+
+
+def write_nav_level(infos, level, args):
+    """
+    Write DTS navigation structure JSON file for level.
+    """
+    nav_struct = {
+        '@context': {
+            '@vocab': 'https://www.w3.org/ns/hydra/core#',
+            'dts': 'https://w3id.org/dts/api#'
+        },
+        '@id': f"{args.url_prefix}/navigation/?id={args.docid}&level={level}",
+        'dts:citeDepth': args.cite_depth,
+        'dts:level': level,
+        'member': [{'dts:ref': ref} for ref in get_div_ids(infos, level)],
+        'dts:passage': f"{args.url_prefix}/documents/?id={args.docid}{{&ref}}",
+        'dts:parent': None
+    }
+    write_json_document(nav_struct, level, args)
+
+
+def write_nav_ref(infos, ref, level, args):
+    """
+    Write DTS navigation structure JSON file for ref.
+    """
+    nav_struct = {
+        '@context': {
+            '@vocab': 'https://www.w3.org/ns/hydra/core#',
+            'dts': 'https://w3id.org/dts/api#'
+        },
+        '@id': f"{args.url_prefix}/navigation/?id={args.docid}&ref={ref}&level={level}",
+        'dts:citeDepth': args.cite_depth,
+        'dts:level': level,
+        'member': [{'dts:ref': ref} for ref in get_div_ids(infos, level)],
+        'dts:passage': f"{args.url_prefix}/documents/?id={args.docid}{{&ref}}",
+        'dts:parent': None
+    }
+    write_json_document(nav_struct, level, args)
+
+
+def write_navigation(infos, args):
+    """
+    Write DTS navigation structure JSON files.
+    """
+    args.cite_depth = get_div_maxlevel(infos, 0)
+    # write navigation for levels
+    for level in range(1, args.cite_depth+1):
+        write_nav_level(infos, level, args)
+   
+    # write navigation for refs
+    
+    
 ##
 ## main
 ##
@@ -155,16 +257,32 @@ def main():
                       help='TEI XML input file.')
     argp.add_argument('-b', '--base-dir', dest='basedir', default='dts-dir', 
                       help='DTSflat output base directory.')
+    argp.add_argument('-i', '--document-id', dest='docid', 
+                      help='DTS main document id (default: inputfile).')
+    argp.add_argument('--gen-id-prefix', dest='genid_prefix', default='genid',
+                      help='Prefix for generated xml-ids.')
+    argp.add_argument('-u', '--url-prefix', dest='url_prefix', default='/dts',
+                      help='DTS API URL prefix.')
  
     args = argp.parse_args()
     
     # set up 
     logging.basicConfig(level=args.loglevel)
-    args.id_cnt = 1
+    if args.docid is None:
+        docid = args.inputfile.lower().replace('.tei', '').replace('.xml', '')
+        args.docid = docid
+        
+    # global counter for generated ids
+    args.genid_cnt = 1
 
+    # load and process inputfile into document endpoint structure
     doc = load_xml_file(args)
-    info = parse_tei(doc, args)
-    logging.debug(f"infos={info}")
+    logging.info("Parsing TEI document and creating document endpoint structure...")
+    info = parse_tei_doc(doc, args)
+    #logging.debug(f"infos={info}")
+    # write navigation endpoint structure
+    logging.info("Creating navigation endpoint structure...")
+    write_navigation(info, args)
 
 
 if __name__ == '__main__':
